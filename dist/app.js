b0c12f644043ae15aa5b31b8af5ee172584d421c
Hironaka <j870614@gmail.com>
2023-04-04 13:28:42 +0800

    feat: 🎸 新增 Node.js 伺服器

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,17 @@
 "use strict";
-const a = '123';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const app = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { method, url } = req;
+    console.log(method, url);
+});
+exports.app = app;
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,6 @@
-const a:string = '123';
+import http from 'http';
+
+export const app =  async (req: http.IncomingMessage, res: http.ServerResponse) => {
+  const { method, url} = req;
+  console.log(method, url);
+};
