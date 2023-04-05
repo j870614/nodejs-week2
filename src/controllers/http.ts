ea6cbb9fa5ce549a1f2af61530c2a672216ba1a6
Hironaka <j870614@gmail.com>
2023-04-05 21:31:28 +0800

    feat: 🎸 新增 http controller

---
+++ src/controllers/http.ts
@@ -0,0 +1,19 @@
+import { IncomingMessage, ServerResponse } from 'http';
+import headers from "../service/headers";
+
+const http = {
+  cors(req: IncomingMessage, res: ServerResponse) {
+    res.writeHead(200, headers);
+    res.end();
+  },
+  notFound(req: IncomingMessage, res: ServerResponse) {
+    res.writeHead(404, headers);
+    res.write(JSON.stringify({
+      status: 'false',
+      message: '無此網絡路由',
+    }));
+    res.end();
+  }
+}
+
+export default http;
