602b97b69091a14466deedf063963f6a31f9032d
Hironaka <j870614@gmail.com>
2023-04-13 13:24:00 +0800

    style: 💄 移除多餘 console.log

--- src/connections/index.ts
+++ src/connections/index.ts
@@ -6,7 +6,6 @@ const DB = process.env.DATABASE!.replace(
   '<password>',
   process.env.DATABASE_PASSWORD!
 );
-console.log(DB);
 // const DB: string = 'mongodb://127.0.0.1:27017/post' //本地資料庫
 
 const connectToDatabase = async () => {
